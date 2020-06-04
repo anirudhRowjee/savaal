9699b3f173347eeca46d6ee1f0c76ffdb3dd771e
Anirudh Rowjee <ani.rowjee@gmail.com>
2020-06-04 19:41:08 +0530

    another go at CI/CD

--- install.sh
+++ install.sh
@@ -6,13 +6,16 @@ if [ $TRAVIS_BRANCH == 'master' ] ; then
   ssh-add ~/.ssh/id_rsa
 
   cd dist
+  echo "In Dist Folder"
+  echo "$(ls -la)"
   git init
 
   git remote add deploy "travis@134.209.153.7:/var/www/html/vue/savaal"
   git config user.name "Travis CI"
   git config user.email "ani.rowjee+travis@gmail.com"
 
-  git add .
+  git add -A
+  echo "$(git status)"
   git commit -m "Deploy"
   git push --force deploy master
 else
